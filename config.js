eb56f04b51f7eb517483d29c814859d0716a40b7
nguyentrongson <37338585+nguyentrongson@users.noreply.github.com>
2024-05-15 14:13:31 +0700

    Add files via upload
    create change

---
+++ config.js
@@ -0,0 +1,13 @@
+const nameGirl = 'Hiền';
+const giftUrl = 'https://www.tiktok.com/@funnipethouse/video/7253019330126957829';
+const eventName = 'Moew Moew';
+const titleCard = 'Moew target 47kí';
+const contentCard = 'Mỗi ngày với Moew luôn là một sự hứng khởi với nhiều niềm vui và điều mới mẻ. Cùng nhau cố gắng nhé Moew Moew ^_^'
+
+// phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
+// Cần hỗ trợ hãy liên hệ: 
+// Mr-Nam http://facebook.com/nam.nodemy
+// Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
+const giftImage = 'hot-girl.png';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
